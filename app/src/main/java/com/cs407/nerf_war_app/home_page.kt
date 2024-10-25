337ca122e7233d0e6c907a4df85499c78b2bb32a
Garrett Hetchler <ghetchler@wisc.edu>
2024-10-24 20:01:09 -0500

    created user sign on page that goes to welcome page

--- app/src/main/java/com/cs407/nerf_war_app/home_page.kt
+++ app/src/main/java/com/cs407/nerf_war_app/home_page.kt
@@ -1,30 +1,20 @@
 package com.cs407.nerf_war_app
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class home_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Find the Sign In button and set a click listener to navigate to home_page.kt
-        val signInButton = findViewById<Button>(R.id.sign_in_button)
-        signInButton.setOnClickListener {
-            // Create an Intent to navigate to HomePageActivity (home_page.kt)
-            val intent = Intent(this, home_page::class.java)
-            startActivity(intent)  // Start the new activity
         }
     }
 }
