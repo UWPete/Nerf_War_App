3016e7a7ecd6518212bc24dd32ac1ca8910ec478
Garrett Hetchler <ghetchler@wisc.edu>
2024-11-20 15:31:57 -0600

    updated about screen

--- AboutScreen.js
+++ AboutScreen.js
@@ -1,12 +1,19 @@
-// AboutScreen.js
-
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 
 const AboutScreen = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.textField}>Hello World</Text>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <Text style={styles.header}>About Nerf War</Text>
+                <Text style={styles.textField}>
+                    We created this app so that people everywhere could enjoy the fun of playing Senior Assassin.
+                    We began working on this project when we noticed that there was no good way to play this game with our friends and were stuck using inferior alternatives.
+                    We wanted an easy, convenient way to play and connect with our classmates.
+                    This app allows you to take control and create your own games or join an existing game to play with your friends, coworkers, or other people in your area using only a few clicks.
+                </Text>
+                <Image source={require('./assets/favicon.png')} style={styles.image} />
+            </ScrollView>
         </View>
     );
 };
@@ -16,13 +23,29 @@ export default AboutScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: '#f0f8ff',
+    },
+    scrollContainer: {
+        padding: 20,
         alignItems: 'center',
-        backgroundColor: '#f5f5f5', // Light background color
+    },
+    image: {
+        width: 300,
+        height: 300,
+        resizeMode: 'contain',
+        marginBottom: 20,
+    },
+    header: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#34495e',
+        marginBottom: 20,
+        textAlign: 'center',
     },
     textField: {
-        fontSize: 24,
-        color: '#2c3e50', // Dark text color
-        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#34495e',
+        textAlign: 'justify',
+        lineHeight: 28,
     },
 });
